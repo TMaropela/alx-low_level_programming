36c65e03f0ded8ff3088cb931c5fa5d07a717621
TMaropela <t.maropela@gmail.com>
2023-09-26 14:16:52 -0700

    Fixed betty error

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - returning the node at the desired index in our linked list
+ * get_nodeint_at_index - returning node at desired index in our linked list
  * @head: 1st node in the linked list
  * @index: index of the returned node
  * Return: pointer to the node we search for, or NULL
