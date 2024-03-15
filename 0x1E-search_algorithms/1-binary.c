b5d43decd387d428748110d05362cfb7e3f9e1fa
TMaropela <t.maropela@gmail.com>
2024-03-15 14:27:19 -0700

    Added advanced tasks

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,37 +1,43 @@
 #include "search_algos.h"
 
 /**
- * print_subarray - subroutine to print out the elements of an array
- * @array: array to print
- * @size: length of array
+ * binary_search - Function that searches for value in sorted integer array
+ * @array: array to search through
+ * @size: Size of the array
+ * @value: Value to search
+ *
+ * Return: index of value, -1 if NULL or not found
  */
-void print_subarray(int *array, size_t size)
-{
-	printf("Searching in array: %d", *array++);
-	while (--size)
-		printf(", %d", *array++);
-	printf("\n");
-}
 
-/**
- * binary_search - binary search for element in array
- * Return: index of element equal to `value`, or -1 if absent or array is
- * NULL or empty.
- */
 int binary_search(int *array, size_t size, int value)
 {
-	int k;
-	size_t mid = (size - 1) / 2;
+	int i, high, low, mid;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
-	print_subarray(array, size);
-	if (value == array[mid])
-		return (mid);
-	if (value < array[mid])
-		return (binary_search(array, mid + 1, value));
-	k = binary_search(array + mid + 1, size - mid - 1, value);
-	if (k == -1)
-		return (-1);
-	return (mid + 1 + k);
+
+	low = 0;
+	high = size - 1;
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		printf("Searching in array: ");
+
+		for (i = low; i <= high; i++)
+		{
+			if (i == high)
+				printf("%i\n", array[i]);
+			else
+				printf("%i, ", array[i]);
+		}
+
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			return (mid);
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/listint/print_list.c
+++ 0x1E-search_algorithms/listint/print_list.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "search_algos.h"
-
 /**
  * print_list - Prints the content of a listint_t
  *
